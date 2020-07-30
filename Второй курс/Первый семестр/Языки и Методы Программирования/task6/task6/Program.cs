3ddc6e57233d7b2599550fb74cc83bdf7baec2d3
mefolder <verybigfolder@yandex.ru>
2020-07-31 04:19:14 +0500

    Remove Weird stuff

--- Второй курс/Первый семестр/Языки и Методы Программирования/task6/task6/Program.cs
+++ Второй курс/Первый семестр/Языки и Методы Программирования/task6/task6/Program.cs
@@ -21,36 +21,31 @@ namespace task6
         }
         public static void Main(string[] args)
         {
-            string a = Console.ReadLine();
-            var ar = a.ToCharArray();
-            Array.Reverse(ar);
-            if (CompareArrays(ar, a.ToCharArray())) Console.WriteLine("Полиндром");
-            else Console.WriteLine("Не");
-            //int n = 0;
-            //Console.WriteLine("Введите размерность матрицы N");
-            //try
-            //{
-            //    n = int.Parse(Console.ReadLine());
-            //}
-            //catch (Exception ex)
-            //{
-            //    Console.WriteLine(ex.Message);
-            //    Environment.Exit(0);
-            // }
-            //int[,] ar = new int[n, n];
-            //for (int i = 0; i != ar.GetLength(0); i++)
-            //{
-            //    ar[i, i] = i + 1;
-            //    ar[i, ar.GetLength(1) - 1 - i] = n - i;
-            //}
-            //for (int i = 0; i != ar.GetLength(0); i++)
-            //{
-            //    for (int j = 0; j != ar.GetLength(1); j++)
-            //    {
-            //        Console.Write(ar[i, j] + " ");
-            //    }
-            //    Console.WriteLine();
-            //}
+            int n = 0;
+            Console.WriteLine("Введите размерность матрицы N");
+            try
+            {
+               n = int.Parse(Console.ReadLine());
+            }
+            catch (Exception ex)
+            {
+               Console.WriteLine(ex.Message);
+               Environment.Exit(0);
+            }
+            int[,] ar = new int[n, n];
+            for (int i = 0; i != ar.GetLength(0); i++)
+            {
+               ar[i, i] = i + 1;
+               ar[i, ar.GetLength(1) - 1 - i] = n - i;
+            }
+            for (int i = 0; i != ar.GetLength(0); i++)
+            {
+               for (int j = 0; j != ar.GetLength(1); j++)
+               {
+                   Console.Write(ar[i, j] + " ");
+               }
+               Console.WriteLine();
+            }
         }
     }
 }

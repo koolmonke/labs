234a4dac3e2ae36eef5833b848576412368b986c
mefolder <verybigfolder@yandex.ru>
2020-09-11 17:05:05 +0500

    Форматирование

--- Третий курс/Первый семестр/Програмирование в Матпакетах/task6.m
+++ Третий курс/Первый семестр/Програмирование в Матпакетах/task6.m
@@ -1,17 +1,17 @@
 function Y = task6 (arr)
   negative_indexes = find(arr < 0);
   positive_indexes = find(arr > 0);
-  to_delete=[]; % Нужные нам индексы negative_indexes(1) и positive_indexes(end)
+  to_delete = []; % Нужные нам индексы negative_indexes(1) и positive_indexes(end)
   
   if !isempty(negative_indexes) % Делаем проверки на то что такие индексы вообще существуют
   to_delete = [negative_indexes(1)];
   end;
   
   if !isempty(positive_indexes)
-  to_delete=[to_delete positive_indexes(end)]
+  to_delete = [to_delete positive_indexes(end)]
   end;
 
-  arr(to_delete)=[];
-  Y=arr;
+  arr(to_delete) = [];
+  Y = arr;
 
 endfunction

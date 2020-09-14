f1b6e800a52cb263e149e09845fdacf481e8c2c5
mefolder <verybigfolder@yandex.ru>
2020-09-14 12:41:23 +0500

    Сделал 5

---
+++ Третий курс/Первый семестр/Програмирование в Матпакетах/task5/task5.m
@@ -0,0 +1,20 @@
+function task5()
+    A = readmatrix('task5_input.csv')
+    m = geomean(A)
+    file = 'task5_output.csv';
+
+    fid = fopen(file, 'wt');
+    fprintf(fid, 'ИСХОДНАЯ МАТРИЦА\n');
+    
+    fclose(fid);
+    writematrix(A, file, 'WriteMode', 'append');
+    
+    for i=1:length(A)
+        A(i,i)=m(i);
+    end
+    fid = fopen(file, 'at');
+    fprintf(fid, '\nПРЕОБРАЗОВАННАЯ МАТРИЦА\n');
+    fclose(fid);
+
+    writematrix(A, file, 'WriteMode', 'append');
+end

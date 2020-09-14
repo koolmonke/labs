a2044438c24242d5c0c75668feb946a85497a64e
mefolder <verybigfolder@yandex.ru>
2020-09-14 19:21:03 +0500

    Оптимизация второго

--- Третий курс/Первый семестр/Програмирование в Матпакетах/task2.m
+++ Третий курс/Первый семестр/Програмирование в Матпакетах/task2.m
@@ -3,17 +3,14 @@
     S = 1;
     sign = true;
     temp = 1;
-    counter_index = 1;
     even_iter = 5; % Четные
     odd_iter = 2; % Не Четные
     
-    while temp > eps
+    while abs(temp) > eps
         temp = even_iter/odd_iter * x * temp;
 
         table = [table; temp S];
         
-        
-        counter_index = counter_index + 1;
         odd_iter = odd_iter + 2;
         even_iter = even_iter + 2;
         if sign

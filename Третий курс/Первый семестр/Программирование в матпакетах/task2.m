90717971436d5714280e865126b635e6ec4d234d
mefolder <verybigfolder@yandex.ru>
2020-09-15 16:10:33 +0500

    Исправил запись знака элемента ряда в таблицу

--- Третий курс/Первый семестр/Программирование в матпакетах/task2.m
+++ Третий курс/Первый семестр/Программирование в матпакетах/task2.m
@@ -1,24 +1,19 @@
 	function S = task2(x)
     table = [];
     S = 1;
-    sign = true;
     temp = 1;
     even_iter = 5; % Четные
     odd_iter = 2; % Не Четные
     
     while abs(temp) > eps
-        temp = even_iter/odd_iter * x * temp;
+        temp = even_iter/odd_iter * -x * temp;
 
         table = [table; temp S];
         
         odd_iter = odd_iter + 2;
         even_iter = even_iter + 2;
-        if sign
-            S = S - temp;
-        else
-            S = S + temp;
-        end
-        sign = ~sign;
+
+        S = S + temp;
     end
     
     T = array2table(table, 'VariableNames', {'Член ряда', 'Сумма членов ряда'})

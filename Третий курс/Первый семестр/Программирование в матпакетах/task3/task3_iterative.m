68e72133449588dc9cc39b5b237f2de04d0240c8
mefolder <verybigfolder@yandex.ru>
2020-09-24 15:22:59 +0500

    Сделал тупой вывод

--- Третий курс/Первый семестр/Программирование в матпакетах/task3/task3_iterative.m
+++ Третий курс/Первый семестр/Программирование в матпакетах/task3/task3_iterative.m
@@ -1,7 +1,7 @@
 function count = task3_iterative()
   arr = readmatrix('task3_input.csv'); % Читаем одномерный массив
   for i = 1:length(arr)
-      fprintf('index %d value %d\n', i, arr(i))
+      fprintf('A[%d]=%d\n', i, arr(i))
   end
   count = 0;
   for i = 1:length(arr)

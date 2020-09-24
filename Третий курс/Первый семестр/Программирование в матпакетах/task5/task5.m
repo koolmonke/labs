2975f5d8cc49fab86c14db0d770c859f84386917
mefolder <verybigfolder@yandex.ru>
2020-09-24 15:16:08 +0500

    Добавил проверку на квадратность матрицы

--- Третий курс/Первый семестр/Программирование в матпакетах/task5/task5.m
+++ Третий курс/Первый семестр/Программирование в матпакетах/task5/task5.m
@@ -1,25 +1,29 @@
 function task5()
-    task5_inner(@geomean, 'task5_output_matlab.csv')
-    task5_inner(@my_geomean, 'task5_output_iter.csv')
+    task5_inner(@geomean,'task5_input.csv', 'task5_output_matlab.csv')
+    task5_inner(@my_geomean, 'task5_input.csv', 'task5_output_iter.csv')
 end
 
-function task5_inner(geomean_func, file)
-    A = readmatrix('task5_input.csv');
-
-    fid = fopen(file, 'wt');
+function task5_inner(geomean_func, file_in, file_out)
+    A = readmatrix(file_in);
+    [num_rows,num_columns] = size(A);
+    if num_rows ~= num_columns
+        return;
+    end
+    
+    fid = fopen(file_out, 'wt');
     fprintf(fid, 'ИСХОДНАЯ МАТРИЦА\n');
     
     fclose(fid);
-    writematrix(A, file, 'WriteMode', 'append');
+    writematrix(A, file_out, 'WriteMode', 'append');
     
     m = geomean_func(A);
     for i=1:length(A)
         A(i,i)=m(i);
     end
     
-    fid = fopen(file, 'at');
+    fid = fopen(file_out, 'at');
     fprintf(fid, '\nПРЕОБРАЗОВАННАЯ МАТРИЦА\n');
     fclose(fid);
 
-    writematrix(A, file, 'WriteMode', 'append');
+    writematrix(A, file_out, 'WriteMode', 'append');
 end

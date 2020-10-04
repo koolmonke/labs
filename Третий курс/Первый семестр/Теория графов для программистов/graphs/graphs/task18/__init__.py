5853c15a9b105353ba9e17f084d3a82c2f46d008
mefolder <verybigfolder@yandex.ru>
2020-10-05 01:24:11 +0500

    Другая идея для работы с графом

--- Третий курс/Первый семестр/Теория графов для программистов/graphs/graphs/task18/__init__.py
+++ Третий курс/Первый семестр/Теория графов для программистов/graphs/graphs/task18/__init__.py
@@ -1,30 +1,32 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import Dict, Set
 
 from pprint import pprint
 
 
 @dataclass(frozen=True)
-class Watchman:
+class Duty:
     start: int
     end: int
+
+    on_duty_counter: int = field(default=0)
 
     def __post_init__(self):
         assert self.start < self.end, "start < end"
 
-    def __contains__(self, key: "Watchman") -> bool:
+    def __contains__(self, key: "Duty") -> bool:
         duty_range = range(self.start, self.end)
         return key.start in duty_range and key.end in duty_range
 
 
-Graph = Dict[Watchman, Set[Watchman]]
+Graph = Dict[Duty, Set[Duty]]
 
 
 @dataclass
 class TimeTable:
     table: Graph
 
-    head: Watchman
+    head: Duty
 
     def __post_init__(self):
         self.table = self.rebalance_graph(self.table)
@@ -37,33 +39,30 @@ class TimeTable:
 
         return graph
 
-    def append_watch(self, watchman: Watchman) -> "TimeTable":
+    def append(self, duty: Duty) -> "TimeTable":
         table = self.table.copy()
         for key, value in table.items():
-            if watchman in key:
-                value.add(watchman)
+            if duty in key:
+                value.add(duty)
 
-        table[watchman] = set()
+        table[duty] = set()
         return TimeTable(self.rebalance_graph(table), self.head)
 
-    def __getitem__(self, key: Watchman) -> "TimeTable":
+    def __getitem__(self, key: Duty) -> "TimeTable":
         return self.__inner_change_head(key)
 
-    def __inner_change_head(self, move_to: Watchman) -> "TimeTable":
+    def __inner_change_head(self, move_to: Duty) -> "TimeTable":
         if move_to in self.table[self.head]:
             return TimeTable(self.table, move_to)
         else:
             raise ValueError(f"There no path between {self.head} and {move_to}")
 
-    def step_a(self) -> bool:
-        return all(map(lambda duties: len(duties) >= 2, self.table.values()))
-
 
 def main() -> None:
     t = (
-        TimeTable({Watchman(0, i): set() for i in range(1, 5)}, Watchman(0, 4))
-        .append_watch(Watchman(1, 4))
-        .append_watch(Watchman(2, 3))
+        TimeTable({Duty(0, i): set() for i in range(1, 5)}, Duty(0, 4))
+        .append(Duty(1, 4))
+        .append(Duty(2, 3))
     )
     pprint(t.table)
 

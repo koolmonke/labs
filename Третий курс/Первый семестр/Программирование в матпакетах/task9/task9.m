5c58b2cfca4b46580b3bbae6c2efe041de9a46c8
mefolder <verybigfolder@yandex.ru>
2020-10-13 12:58:08 +0500

    Update task9.m

--- Третий курс/Первый семестр/Программирование в матпакетах/task9/task9.m
+++ Третий курс/Первый семестр/Программирование в матпакетах/task9/task9.m
@@ -6,26 +6,26 @@ function task9()
     disp('Domain of the function is: ');
     disp('x > 0');
 
-    disp('# 2. One-sided limits');
-    disp('Limit at plus infinity');
+    disp('# 2. Односторонние приделы');
+    disp('Придел к бесконечности');
     disp(limit(y, x, inf));
 
-    disp('Right side limit at zero');
+    disp('Предел справа к нулю');
     disp(limit(y, x, 0, 'right'));
 
-    disp('# 3. Intersections with axis');
+    disp('# 3. пересечение с осями');
     disp('Ox');
     disp(vpasolve(y == 0, x));
     disp('Oy');
     disp(y(0));
 
-    disp('# 4. Parity');
+    disp('# 4. Четность');
     disp(OddOrEven(y));
 
-    disp('# 5. Is periodic?');
+    disp('# 5. Периодическая?');
     disp('No :)');
 
-    disp('# 6. Extremums');
+    disp('# 6. Экстремумы');
     dy(x) = diff(y, x);
     extr = solve(dy == 0, x);
 
@@ -35,14 +35,14 @@ function task9()
     fprintf('     %0d      %d      %0d     \n', sgn_m, extr, sgn_p);
 
     if sgn_m < sgn_p
-        disp('Local minimum: ');
+        disp('Локальный минимум: ');
     else
-        disp('Local maximum: ');
+        disp('Локальный максимум: ');
     end
 
     disp(double(y(extr)));
 
-    disp('# 7. Convexity')
+    disp('# 7. Выпуклость')
 
     ddy = diff(dy, x);
     extr = solve(ddy == 0, x);
@@ -58,27 +58,27 @@ function task9()
     end
 
     if sgn_m > 0
-        fprintf('Strictly convex when x > %d\n', extr);
+        fprintf('Строго выпукло при x > %d\n', extr);
     else
-        fprintf('Convex when x > %d\n', extr');
+        fprintf('Выпукла при x > %d\n', extr');
     end
 
-    disp('# 8. Horizontal and vertiacal asymptotes');
-    disp('No horizontal limits, because limit to +inf is -inf and -inf is undefined');
+    disp('# 8. Горизонт и вертикальные асимптоты');
+    disp('Нет горизонтальных пиделов, потому что предел к +inf это -inf и -inf это undefined');
 
     disp('y = k*x + b');
     k = limit(y / x, x, inf);
     b = limit(y - k * x, x, inf);
     fprintf('y = %d * x + %d\n', k, b);
 
-    disp('No inclined asymptotes');
+    disp('Нет асимптот');
 
-    disp('# 9. Plot');
-    a = input('Left edge of interval to plot\n');
-    b = input('Right edge of interval to plot\n');
+    disp('# 9. График');
+    a = input('Левый край интервала для постройки\n');
+    b = input('Правый край интервала для постройки\n');
     task9_1(a, b)
 
-    disp('# 10. Taylor series');
+    disp('# 10. ряд Тейлора');
 
     disp('Taylor expansion at x = 1');
     pretty(taylor(y, x, 1));
@@ -87,11 +87,11 @@ end
 function isOdd = OddOrEven(symfunc)
 
     if symfunc(-1) == symfunc(1)
-        isOdd = 'Function is odd';
+        isOdd = 'Функция четная';
     elseif symfunc(-1) == -symfunc(1)
-        isOdd = 'Function is even';
+        isOdd = 'Функция нечетная';
     else
-        isOdd = 'Function isn`t odd or even';
+        isOdd = 'Функция ни четная ни нечетная';
     end
 
 end

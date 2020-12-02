2cf020084e420110dd31cfa3b5b4f2583ffff23b
mefolder <verybigfolder@yandex.ru>
2020-12-02 19:31:53 +0500

    Сделал таблички в mysql

---
+++ Третий курс/Первый семестр/Базы данных/Database/mysql-init-files/init.sql
@@ -0,0 +1,68 @@
+create database kinos;
+
+use kinos;
+
+create table workers(
+    id int not null auto_increment,
+    primary key (id),
+    lastname text,
+    firstname text not null,
+    middlename text
+);
+
+create table movies(
+    id int not null auto_increment,
+    primary key (id),
+    duration time not null,
+    begin_film_release datetime not null,
+    end_film_release datetime not null,
+    distributor text not null
+);
+
+create table cinema_halls(
+    id int not null auto_increment,
+    primary key (id),
+    name_of_hall nvarchar(50) not null,
+    amount_of_seats_total int not null,
+    description text,
+    number_of_rows int not null,
+    number_of_seats int not null
+);
+
+create table seats(
+    id int not null auto_increment,
+    primary key (id),
+    cinema_halls_id int not null references cinema_halls(id),
+    foreign key (cinema_halls_id) references cinema_halls(id),
+    row_index int not null,
+    seat_index int not null
+);
+
+create table movie_shows(
+    id int not null auto_increment,
+    primary key (id),
+    start_of_show datetime not null,
+    movies_id int not null references movies(id),
+    foreign key (movies_id) references movies(id)
+);
+
+create table ops_on_tickets(
+    id int not null auto_increment,
+    primary key (id),
+    date_of_op datetime not null,
+    op_type enum('бронь', 'оплата', 'отмена', 'возврат', 'проход'),
+    workers_id int not null references workers(id),
+    foreign key (workers_id) references workers(id)
+);
+
+create table tickets(
+    id int not null auto_increment,
+    primary key (id),
+    movie_show_id int not null references movie_shows(id),
+    foreign key (movie_show_id) references movie_shows(id),
+    seats_id int not null references seats(id),
+    foreign key (seats_id) references seats(id),
+    is_payed bool not null default (false),
+    is_booked bool not null default (false),
+    type_of_payment enum('картой', 'наличный расчет')
+)
--- Третий курс/Первый семестр/Базы данных/Database/php/index.php
+++ Третий курс/Первый семестр/Базы данных/Database/php/index.php
@@ -1,9 +1,25 @@
+<!DOCTYPE html>
+<html lang="RU-ru">
+<body>
+
 <?php
+$colors = array("red", "green", "blue", "yellow");
 
-// Show all information, defaults to INFO_ALL
-phpinfo();
+foreach ($colors as $value) {
+    echo "$value <br>";
+}
+?>
 
-// Show just the module information.
-// phpinfo(8) yields identical results.
-phpinfo(INFO_MODULES);
+</body>
+</html>
 
+utt = a2 uxx + Ax + B, 0 < x < l, t > 0,
+ux (0, t) = ux (l, t) = 0,
+u(x, 0) = U , ut (x, 0) = V ,
+а)A = 2, B = 1, U = 1, V = 0
+б)A = 1, B = 1, U = 2, V = 1
+в)A = 1, B = 0, U = 1, V = 2.
+
+ut = a2 uxx + x + 1, 0 < x < 1, t > 0,
+u(0, t) = 0, u(1, t) = 1,
+u(x, 0) = x.

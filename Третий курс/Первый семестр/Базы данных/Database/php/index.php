284bd6e52e7995f8d883d3586e773d2519070891
mefolder <verybigfolder@yandex.ru>
2020-12-17 21:51:22 +0500

    Move index.php to movies.php, seats.php to index.php

--- Третий курс/Первый семестр/Базы данных/Database/php/index.php
+++ Третий курс/Первый семестр/Базы данных/Database/php/index.php
@@ -1,29 +1,39 @@
 <!DOCTYPE html>
 <html lang="RU-ru">
 <head>
-    <title>Фильмы</title>
+    <title>Места</title>
 </head>
 <body>
 
 <?php
-
 $db = new PDO('mysql:host=db;dbname=kinos', 'devuser', 'devpass');
-echo "Все фильмы:";
+echo "Все места:";
 echo '<table border="1" width="100%" cellpadding="5">';
-foreach($db->query('select * from movies') as $row) {
+echo '<tr>';
+echo "<th>id</th>";
+echo "<th>Номер ряда</th>";
+echo "<th>Номер места</th>";
+echo "<th style='color: blue' '>Номер зала</th>";
+echo "<th style='color: blue' '>Название зала</th>";
+echo "<th style='color: blue' '>Описание зала</th>";
+echo "</tr>";
+foreach ($db->query('select seats.id as seats_pk, cinema_halls_id, row_index, seat_index, name_of_hall, description from kinos.seats join cinema_halls ch on ch.id = seats.cinema_halls_id order by seats_pk') as $row) {
     echo '<tr>';
-    echo "<th>{$row['id']}</th>";
-    echo "<th>{$row['title']}</th>";
-    echo "<th>{$row['duration']}</th>";
-    echo "<th>{$row['begin_film_release']}</th>";
-    echo "<th>{$row['end_film_release']}</th>";
-    echo "<th>{$row['distributor']}</th>";
+    echo "<th>{$row['seats_pk']}</th>";
+    echo "<th>{$row['row_index']}</th>";
+    echo "<th>{$row['seat_index']}</th>";
+    echo "<th style='color: blue'>{$row['cinema_halls_id']}</th>";
+    echo "<th style='color: blue'>{$row['name_of_hall']}</th>";
+    echo "<th style='color: blue'>{$row['description']}</th>";
     echo "</tr>";
 }
-echo "</table>"
-
+echo "</table>";
 
 ?>
+<p><a target="_blank" href="submit_seats.php"> Добавить место </a></p>
+<p><a target="_blank" href="delete_seats.php"> Удалить место </a></p>
+<p><a target="_blank" href="update_seats.php"> Изменить место </a></p>
 
+<p><a target="_blank" href="queries.php"> Запросы к БД </a></p>
 </body>
 </html>
---
+++ Третий курс/Первый семестр/Базы данных/Database/php/movies.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="RU-ru">
+<head>
+    <title>Фильмы</title>
+</head>
+<body>
+
+<?php
+
+$db = new PDO('mysql:host=db;dbname=kinos', 'devuser', 'devpass');
+echo "Все фильмы:";
+echo '<table border="1" width="100%" cellpadding="5">';
+foreach($db->query('select * from movies') as $row) {
+    echo '<tr>';
+    echo "<th>{$row['id']}</th>";
+    echo "<th>{$row['title']}</th>";
+    echo "<th>{$row['duration']}</th>";
+    echo "<th>{$row['begin_film_release']}</th>";
+    echo "<th>{$row['end_film_release']}</th>";
+    echo "<th>{$row['distributor']}</th>";
+    echo "</tr>";
+}
+echo "</table>"
+
+
+?>
+
+</body>
+</html>

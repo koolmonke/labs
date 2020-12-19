78f5c93a3c3494770ce8c298c53556f8db3a7101
mefolder <verybigfolder@yandex.ru>
2020-12-20 01:42:43 +0500

    Fix init

--- Третий курс/Первый семестр/Базы данных/Database/mysql-init-files/00-init.sql
+++ Третий курс/Первый семестр/Базы данных/Database/mysql-init-files/00-init.sql
@@ -44,6 +44,19 @@ create table movie_shows(
     foreign key (movies_id) references movies(id)
 );
 
+create table tickets(
+                        id int not null auto_increment,
+                        primary key (id),
+                        date_of_issue date,
+                        movie_show_id int not null references movie_shows(id),
+                        foreign key (movie_show_id) references movie_shows(id),
+                        seats_id int not null references seats(id),
+                        foreign key (seats_id) references seats(id),
+                        is_payed bool not null default (false),
+                        is_booked bool not null default (false),
+                        type_of_payment enum('картой', 'наличный расчет')
+);
+
 create table ops_on_tickets(
     id int not null auto_increment,
     primary key (id),
@@ -53,17 +66,4 @@ create table ops_on_tickets(
     foreign key (workers_id) references workers(id),
     ticket_id int not null,
     foreign key (ticket_id) references tickets(id)
-);
-
-create table tickets(
-    id int not null auto_increment,
-    primary key (id),
-    date_of_issue date,
-    movie_show_id int not null references movie_shows(id),
-    foreign key (movie_show_id) references movie_shows(id),
-    seats_id int not null references seats(id),
-    foreign key (seats_id) references seats(id),
-    is_payed bool not null default (false),
-    is_booked bool not null default (false),
-    type_of_payment enum('картой', 'наличный расчет')
 );

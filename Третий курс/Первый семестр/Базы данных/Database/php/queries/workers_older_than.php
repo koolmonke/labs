ae2678de529b884271196447fc631548a1401474
mefolder <verybigfolder@yandex.ru>
2020-12-21 17:29:49 +0500

    Fix typo

--- Третий курс/Первый семестр/Базы данных/Database/php/queries/workers_older_than.php
+++ Третий курс/Первый семестр/Базы данных/Database/php/queries/workers_older_than.php
@@ -3,8 +3,8 @@
 <div class="main_content">
     <?php
     include "../Utils.php";
-    $query = "select firstname, lastname, truncate(datediff(curdate(), dob) / 365.25, 0) as age from workers;";
-    echo Utils::renderSelectQueryToTable($query, [$_POST["length"]]);
+    $query = "select firstname, lastname, truncate(datediff(curdate(), dob) / 365.25, 0) as age from workers where truncate(datediff(curdate(), dob) / 365.25, 0) > ?;";
+    echo Utils::renderSelectQueryToTable($query, [$_POST["age"]]);
     echo "<p>Запрос: $query</p>";
     ?>
     <a class="buttons" href="../queries.php">Назад</a>

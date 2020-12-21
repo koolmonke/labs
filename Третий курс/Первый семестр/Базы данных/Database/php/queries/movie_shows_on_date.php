8e25d1403fbe5dd3c7fdd6355edc05a26222fcec
mefolder <verybigfolder@yandex.ru>
2020-12-21 19:47:05 +0500

    Make query more complex

--- Третий курс/Первый семестр/Базы данных/Database/php/queries/movie_shows_on_date.php
+++ Третий курс/Первый семестр/Базы данных/Database/php/queries/movie_shows_on_date.php
@@ -3,7 +3,7 @@
 <div class="main_content">
     <?php
     include "../Utils.php";
-    $query = "select * from movie_shows where start_of_show=?";
+    $query = "select title, start_of_show, name_of_hall from movie_shows join cinema_halls ch on ch.id = movie_shows.cinema_halls_id join movies m on m.id = movie_shows.movies_id where date(start_of_show) = ?";
     echo Utils::renderSelectQueryToTable($query, [$_POST["date"]]);
     echo "<p>Запрос: $query</p>";
     ?>

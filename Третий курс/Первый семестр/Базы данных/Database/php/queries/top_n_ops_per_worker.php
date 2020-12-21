285ab41b2206cb356da4735d2ee8f956a318400c
mefolder <verybigfolder@yandex.ru>
2020-12-21 17:29:35 +0500

    Avoid sql injections

--- Третий курс/Первый семестр/Базы данных/Database/php/queries/top_n_ops_per_worker.php
+++ Третий курс/Первый семестр/Базы данных/Database/php/queries/top_n_ops_per_worker.php
@@ -3,8 +3,8 @@
 <div class="main_content">
     <?php
     include "../Utils.php";
-    $query = "select firstname, lastname, workers_id, count(*) as magnitude from ops_on_tickets join workers w on w.id = ops_on_tickets.workers_id group by workers_id order by magnitude desc limit {$_POST['n']};";
-    echo Utils::renderSelectQueryToTable($query, []);
+    $query = "select firstname, lastname, workers_id, count(*) as magnitude from ops_on_tickets join workers w on w.id = ops_on_tickets.workers_id group by workers_id order by magnitude desc limit ?;";
+    echo Utils::renderSelectQueryToTable($query, [$_POST['n']]);
     echo "<p>Запрос: $query</p>";
     ?>
     <a class="buttons" href="../queries.php">Назад</a>

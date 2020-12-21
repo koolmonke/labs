60916cff896a7405ba6bf0c1aaf41096ab2a3cea
mefolder <verybigfolder@yandex.ru>
2020-12-21 19:21:36 +0500

    Code reformat

--- Третий курс/Первый семестр/Базы данных/Database/mysql-init-files/00-init.sql
+++ Третий курс/Первый семестр/Базы данных/Database/mysql-init-files/00-init.sql
@@ -1,69 +1,76 @@
 use kinos;
 
-create table workers(
-    id int not null auto_increment,
+create table workers
+(
+    id        int          not null auto_increment,
     primary key (id),
-    lastname varchar(256) not null,
+    lastname  varchar(256) not null,
     firstname varchar(256) not null,
-    dob date not null
+    dob       date         not null
 );
 
-create table movies(
-    id int not null auto_increment,
+create table movies
+(
+    id                 int          not null auto_increment,
     primary key (id),
-    title varchar(256) not null,
-    duration time not null,
-    begin_film_release datetime not null,
-    end_film_release datetime not null,
-    distributor varchar(256) not null
+    title              varchar(256) not null,
+    duration           time         not null,
+    begin_film_release datetime     not null,
+    end_film_release   datetime     not null,
+    distributor        varchar(256) not null
 );
 
-create table cinema_halls(
-    id int not null auto_increment,
+create table cinema_halls
+(
+    id           int                 not null auto_increment,
     primary key (id),
     name_of_hall varchar(256) unique not null,
-    description varchar(256)
+    description  varchar(256)
 );
 
-create table seats(
-    id int not null auto_increment,
+create table seats
+(
+    id              int not null auto_increment,
     primary key (id),
-    cinema_halls_id int not null references cinema_halls(id),
-    foreign key (cinema_halls_id) references cinema_halls(id),
-    row_index int not null,
-    seat_index int not null
+    cinema_halls_id int not null references cinema_halls (id),
+    foreign key (cinema_halls_id) references cinema_halls (id),
+    row_index       int not null,
+    seat_index      int not null
 );
 
-create table movie_shows(
-    id int not null auto_increment,
+create table movie_shows
+(
+    id              int      not null auto_increment,
     primary key (id),
-    cinema_halls_id int not null,
-    foreign key (cinema_halls_id) references cinema_halls(id),
-    start_of_show datetime not null,
-    movies_id int not null references movies(id),
-    foreign key (movies_id) references movies(id)
+    cinema_halls_id int      not null,
+    foreign key (cinema_halls_id) references cinema_halls (id),
+    start_of_show   datetime not null,
+    movies_id       int      not null references movies (id),
+    foreign key (movies_id) references movies (id)
 );
 
-create table tickets(
-    id int not null auto_increment,
+create table tickets
+(
+    id              int  not null auto_increment,
     primary key (id),
-    date_of_issue date,
-    movie_show_id int not null references movie_shows(id),
-    foreign key (movie_show_id) references movie_shows(id),
-    seats_id int not null references seats(id),
-    foreign key (seats_id) references seats(id),
-    is_payed bool not null default (false),
-    is_booked bool not null default (false),
-    type_of_payment enum('картой', 'наличный расчет')
+    date_of_issue   date,
+    movie_show_id   int  not null references movie_shows (id),
+    foreign key (movie_show_id) references movie_shows (id),
+    seats_id        int  not null references seats (id),
+    foreign key (seats_id) references seats (id),
+    is_payed        bool not null default (false),
+    is_booked       bool not null default (false),
+    type_of_payment enum ('картой', 'наличный расчет')
 );
 
-create table ops_on_tickets(
-    id int not null auto_increment,
+create table ops_on_tickets
+(
+    id         int      not null auto_increment,
     primary key (id),
     date_of_op datetime not null,
-    op_type enum('бронь', 'оплата', 'отмена', 'возврат', 'проход'),
-    workers_id int not null references workers(id),
-    foreign key (workers_id) references workers(id),
-    ticket_id int not null,
-    foreign key (ticket_id) references tickets(id)
+    op_type    enum ('бронь', 'оплата', 'отмена', 'возврат', 'проход'),
+    workers_id int      not null references workers (id),
+    foreign key (workers_id) references workers (id),
+    ticket_id  int      not null,
+    foreign key (ticket_id) references tickets (id)
 );
--- Третий курс/Первый семестр/Базы данных/Database/php/queries/lastnames.php
+++ Третий курс/Первый семестр/Базы данных/Database/php/queries/lastnames.php
@@ -4,7 +4,7 @@
     <?php
     include "../Utils.php";
     $query = "select * from workers where lastname like ?";
-    echo Utils::renderSelectQueryToTable($query, ['%'.$_POST['lastname']]);
+    echo Utils::renderSelectQueryToTable($query, ['%' . $_POST['lastname']]);
     echo "<p>Запрос: $query</p>";
     ?>
     <a class="buttons" href="../queries.php">Назад</a>

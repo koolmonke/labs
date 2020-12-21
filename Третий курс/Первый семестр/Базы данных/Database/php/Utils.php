00151c4b9f966a62bc9a5e1958b0f9d0058ba644
mefolder <verybigfolder@yandex.ru>
2020-12-21 17:29:03 +0500

    Disable prepares in pdo

--- Третий курс/Первый семестр/Базы данных/Database/php/Utils.php
+++ Третий курс/Первый семестр/Базы данных/Database/php/Utils.php
@@ -19,6 +19,7 @@ class Utils
         $options = [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false,
         ];
         return new PDO('mysql:host=db;dbname=kinos', 'devuser', 'devpass', $options);
     }

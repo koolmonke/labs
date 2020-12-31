b07f7e1236825e307237c708786b9edc75cafd06
mefolder <verybigfolder@yandex.ru>
2021-01-01 01:18:05 +0500

    Remove semicolon

--- Третий курс/Первый семестр/Базы данных/Database/php/queries/top_n_ops_per_worker.php
+++ Третий курс/Первый семестр/Базы данных/Database/php/queries/top_n_ops_per_worker.php
@@ -4,7 +4,7 @@
     <?php
     if (!empty($_POST)) {
         include "../Utils.php";
-        $query = "select firstname, lastname, workers_id, count(*) as magnitude from ops_on_tickets join workers w on w.id = ops_on_tickets.workers_id group by workers_id order by magnitude desc limit ?;";
+        $query = "select firstname, lastname, workers_id, count(*) as magnitude from ops_on_tickets join workers w on w.id = ops_on_tickets.workers_id group by workers_id order by magnitude desc limit ?";
         echo Utils::renderSelectQueryToTable($query, [$_POST['n']]);
         echo "<p>Запрос: $query</p>";
     } else {

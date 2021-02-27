33a86407e9e3e3ba74f02b415458e87fbab721be
Arthur Khakimov <verybigfolder@yandex.ru>
2021-02-27 13:54:47 +0500

    Code reformat

--- Третий курс/Второй семестр/Недетерминированные алгоритмы/non-deterministic-algorithms/src/main/kotlin/lab1.kt
+++ Третий курс/Второй семестр/Недетерминированные алгоритмы/non-deterministic-algorithms/src/main/kotlin/lab1.kt
@@ -14,7 +14,7 @@ data class Backpack(val sizeOfBackpack: Weight) {
     fun fill(items: Items): Pair<Items, Int> {
         var currentSize = 0
         val currentItems = mutableListOf<Item>()
-        for (item in items.sortedByDescending { item -> item.weight / item.price }) {
+        for (item in items.sortedByDescending { it.weight / it.price }) {
             if ((currentSize + item.weight) <= sizeOfBackpack) {
                 currentItems.add(item)
                 currentSize += item.weight
@@ -30,8 +30,9 @@ fun main(args: Array<String>) {
         val items = Json.decodeFromString<Items>(File(filename).readText())
         val sizeOfBackpack = args[1].toInt()
         val backpack = Backpack(sizeOfBackpack)
+        val (optimalItemsInBackpack, optimalSize) = backpack.fill(items)
         println(items)
-        println(backpack.fill(items))
+        println("Предметы: $optimalItemsInBackpack, их вес $optimalSize")
     } else {
         println("Первый аргумент путь до файла, второй размер рюкзака")
     }

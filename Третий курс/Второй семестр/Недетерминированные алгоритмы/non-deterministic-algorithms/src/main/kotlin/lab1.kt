ece2905b1de1ac692e280c01a8788b860d2d859a
Arthur Khakimov <verybigfolder@yandex.ru>
2021-02-27 10:37:34 +0500

    Сдал первую лабу

--- Третий курс/Второй семестр/Недетерминированные алгоритмы/non-deterministic-algorithms/src/main/kotlin/lab1.kt
+++ Третий курс/Второй семестр/Недетерминированные алгоритмы/non-deterministic-algorithms/src/main/kotlin/lab1.kt
@@ -3,17 +3,36 @@ import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 import java.io.File
 
+typealias Weight = Int
+
 @Serializable
-data class Item(val price: Int, val weight: Int)
+data class Item(val price: Int, val weight: Weight)
 
 typealias Items = List<Item>
 
-fun random(sizeOfBackpack: Int, items: Items) = takeWhileBackpackIsNotFull(sizeOfBackpack, items.shuffled())
+data class Backpack(val sizeOfBackpack: Weight) {
+    fun fill(items: Items): Pair<Items, Int> {
+        var currentSize = 0
+        val currentItems = mutableListOf<Item>()
+        for (item in items.sortedByDescending { item -> item.weight / item.price }) {
+            if ((currentSize + item.weight) <= sizeOfBackpack) {
+                currentItems.add(item)
+                currentSize += item.weight
+            }
+        }
+        return Pair(currentItems as List<Item>, currentSize)
+    }
+}
 
-fun orderedByPricePerKilo(sizeOfBackpack: Int, items: Items) =
+fun random(sizeOfBackpack: Weight, items: Items) = takeWhileBackpackIsNotFull(sizeOfBackpack, items.shuffled())
+
+fun orderedByPricePerKilo(sizeOfBackpack: Weight, items: Items) =
     takeWhileBackpackIsNotFull(sizeOfBackpack, items.sortedByDescending { it.price / it.weight })
 
-fun takeWhileBackpackIsNotFull(sizeOfBackpack: Int, items: Items): Items =
+fun orderByWeight(sizeOfBackpack: Weight, items: Items) =
+    takeWhileBackpackIsNotFull(sizeOfBackpack, items.sortedBy { it.weight })
+
+fun takeWhileBackpackIsNotFull(sizeOfBackpack: Weight, items: Items): Items =
     items.zip(items.runningFold(0) { acc, item -> acc + item.weight }).takeWhile { it.second < sizeOfBackpack }
         .dropLast(1).map(Pair<Item, Int>::first)
 
@@ -22,9 +41,9 @@ fun main(args: Array<String>) {
         val filename = args[0]
         val items = Json.decodeFromString<Items>(File(filename).readText())
         val sizeOfBackpack = args[1].toInt()
+        val backpack = Backpack(sizeOfBackpack)
+        println(backpack.fill(items))
         println(items)
-        println(orderedByPricePerKilo(sizeOfBackpack, items))
-        println(random(sizeOfBackpack, items))
     } else {
         println("Первый аргумент путь до файла, второй размер рюкзака")
     }

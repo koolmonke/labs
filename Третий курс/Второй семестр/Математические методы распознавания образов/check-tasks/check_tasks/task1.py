83cf82724aadd0bda3031b65c3068096602bcab5
Artur Khakimov <verybigfolder@yandex.ru>
2021-03-05 12:32:56 +0500

    Fix typo

--- Третий курс/Второй семестр/Математические методы распознавания образов/check-tasks/check_tasks/task1.py
+++ Третий курс/Второй семестр/Математические методы распознавания образов/check-tasks/check_tasks/task1.py
@@ -1,7 +1,7 @@
 import json
 from dataclasses import dataclass
 from pathlib import Path
-from typing import List
+from typing import List, Sequence
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -30,7 +30,7 @@ def load_points(path: Path) -> List[Point]:
         return json.load(file, object_hook=lambda point: Point(**point))
 
 
-def fit_circle(points: List[Point]):  # находим по мнк
+def fit_circle(points: Sequence[Point]):  # находим по мнк
     x = np.array([point.x for point in points])
     y = np.array([point.y for point in points])
 
@@ -52,8 +52,8 @@ def fit_circle(points: List[Point]):  # находим по мнк
     sum_y__x_2_plus_y_2 = y__x_2_plus_y_2.sum(dtype=float)
 
     inv_m3b3 = np.linalg.inv(np.array([[sum_x_2, sum_x_y, sum_x],
-                                      [sum_x_y, sum_y_2, sum_y],
-                                      [sum_x, sum_y, len(points)]]))
+                                       [sum_x_y, sum_y_2, sum_y],
+                                       [sum_x, sum_y, len(points)]]))
     m3b1 = np.array([sum_x__x_2_plus_y_2, sum_y__x_2_plus_y_2, sum_x_2_plus_y_2])
     a, b, c = np.dot(inv_m3b3, m3b1)
     return Circle(center=Point(x=a / 2, y=b / 2), radius=np.sqrt(4 * c + a ** 2 + b ** 2) / 2)
@@ -65,7 +65,7 @@ def main():
     circle = fit_circle(points)
     print(circle)
     plt.plot([p.x for p in points], [p.y for p in points], "o")
-    plt.gca().add_patch(plt.Circle(*circle.to_tuple(), color='r', fill=False))
+    plt.gca().add_patch(plt.Circle(*circle.to_tuple(), color="r", fill=False))
     plt.show()
 
 

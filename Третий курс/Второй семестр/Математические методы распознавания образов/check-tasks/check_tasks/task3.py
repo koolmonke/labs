7111559dc4ef2b8490d7050c048f3011663bb21f
Arthur Khakimov <verybigfolder@yandex.ru>
2021-03-15 14:57:47 +0500

    Refactor task3.py

--- Третий курс/Второй семестр/Математические методы распознавания образов/check-tasks/check_tasks/task3.py
+++ Третий курс/Второй семестр/Математические методы распознавания образов/check-tasks/check_tasks/task3.py
@@ -4,15 +4,18 @@ TYPOS = 500
 PAGES = 500
 
 
-def main(symbols: int = 500) -> (int, int):
+def f(symbols: int = 500) -> (int, int):
     minim = TYPOS if TYPOS <= PAGES else 0
-    maxim = 1 if TYPOS <= symbols else ceil(TYPOS/symbols)
+    maxim = 1 if TYPOS <= symbols else ceil(TYPOS / symbols)
 
     return minim, maxim
 
 
-if __name__ == '__main__':
-    inp = int(input('количество символов на странице '))
-    minim, maxim = main(inp)
-
+def main() -> None:
+    char_count = int(input('количество символов на странице '))
+    minim, maxim = f(char_count)
     print(f'от {PAGES - minim} до {PAGES - maxim} страниц не содержат опечатки')
+
+
+if __name__ == '__main__':
+    main()

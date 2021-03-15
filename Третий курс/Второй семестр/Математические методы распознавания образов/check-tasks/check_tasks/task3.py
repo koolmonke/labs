e0511e2e4d9c56c4dfc426486f9f93524fffbb74
Artur Khakimov <verybigfolder@yandex.ru>
2021-03-16 01:06:22 +0500

    Code reformat

--- Третий курс/Второй семестр/Математические методы распознавания образов/check-tasks/check_tasks/task3.py
+++ Третий курс/Второй семестр/Математические методы распознавания образов/check-tasks/check_tasks/task3.py
@@ -1,7 +1,7 @@
 from math import ceil
 
 
-def solve(char_per_page: int = 500, typos: int = 500, pages: int = 500) -> (int, int):
+def solve(char_per_page: int = 500, typos: int = 500, pages: int = 500):
     maxim = min(typos, pages)
     minim = ceil(typos / char_per_page)
 
@@ -9,12 +9,12 @@ def solve(char_per_page: int = 500, typos: int = 500, pages: int = 500) -> (int,
 
 
 def main() -> None:
-    char_per_page = int(input('Количество символов на странице '))
+    char_per_page = int(input("Количество символов на странице "))
     pages = int(input("Кол-во страниц "))
     typos = int(input("Кол-во опечаток "))
 
-    minim, maxim = solve(char_per_page, typos, pages)
-    print(f'от {pages - minim} до {pages - maxim} страниц не содержат опечатки')
+    minimum, maximum = solve(char_per_page, typos, pages)
+    print(f"от {pages - minimum} до {pages - maximum} страниц не содержат опечатки")
 
 
 if __name__ == '__main__':

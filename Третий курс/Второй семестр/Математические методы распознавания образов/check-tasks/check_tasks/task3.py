815097df5b094425b777d580e4db1d4782a78434
Artur Khakimov <verybigfolder@yandex.ru>
2021-03-16 01:57:05 +0500

    Inline solve function

--- Третий курс/Второй семестр/Математические методы распознавания образов/check-tasks/check_tasks/task3.py
+++ Третий курс/Второй семестр/Математические методы распознавания образов/check-tasks/check_tasks/task3.py
@@ -1,20 +1,12 @@
 from math import ceil
 
 
-def solve(char_per_page: int = 500, typos: int = 500, pages: int = 500):
-    maxim = min(typos, pages)
-    minim = ceil(typos / char_per_page)
-
-    return minim, maxim
-
-
 def main() -> None:
     char_per_page = int(input("Количество символов на странице "))
     pages = int(input("Кол-во страниц "))
     typos = int(input("Кол-во опечаток "))
 
-    minimum, maximum = solve(char_per_page, typos, pages)
-    print(f"от {pages - maximum} до {pages - minimum} страниц не содержат опечатки")
+    print(f"от {pages - min(typos, pages)} до {pages - ceil(typos / char_per_page)} страниц не содержат опечатки")
 
 
 if __name__ == '__main__':

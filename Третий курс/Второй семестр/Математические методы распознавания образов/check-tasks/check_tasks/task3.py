106adeb913f47bddf87a47d42e40ac4a6675bd7a
Arthur Khakimov <verybigfolder@yandex.ru>
2021-03-15 15:40:33 +0500

    Refactor task3.py

--- Третий курс/Второй семестр/Математические методы распознавания образов/check-tasks/check_tasks/task3.py
+++ Третий курс/Второй семестр/Математические методы распознавания образов/check-tasks/check_tasks/task3.py
@@ -1,20 +1,20 @@
 from math import ceil
 
-TYPOS = 500
-PAGES = 500
 
-
-def f(symbols: int = 500) -> (int, int):
-    minim = TYPOS if TYPOS <= PAGES else 0
-    maxim = 1 if TYPOS <= symbols else ceil(TYPOS / symbols)
+def solve(char_per_page: int = 500, typos: int = 500, pages: int = 500) -> (int, int):
+    minim = typos if typos <= pages else 0
+    maxim = 1 if typos <= char_per_page else ceil(typos / char_per_page)
 
     return minim, maxim
 
 
 def main() -> None:
-    char_count = int(input('количество символов на странице '))
-    minim, maxim = f(char_count)
-    print(f'от {PAGES - minim} до {PAGES - maxim} страниц не содержат опечатки')
+    char_per_page = int(input('Количество символов на странице '))
+    pages = int(input("Кол-во страниц "))
+    typos = int(input("Кол-во опечаток "))
+
+    minim, maxim = solve(char_per_page, typos, pages)
+    print(f'от {pages - minim} до {pages - maxim} страниц не содержат опечатки')
 
 
 if __name__ == '__main__':

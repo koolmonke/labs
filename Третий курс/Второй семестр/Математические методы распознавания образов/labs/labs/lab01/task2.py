93211c9960f8744a89da72091b2c53d6d9615b20
Artur Khakimov <verybigfolder@yandex.ru>
2021-03-22 18:56:10 +0500

    Вроде решил 2

---
+++ Третий курс/Второй семестр/Математические методы распознавания образов/labs/labs/lab01/task2.py
@@ -0,0 +1,21 @@
+from typing import Sequence
+
+from labs.lab01.task1 import Image, read_test_data, read_train_data, TrainImage
+
+
+def distance(some: Image, other: Image):
+    return ((some.data - other.data) ** 2).sum()
+
+
+def best_distance(db: Sequence[TrainImage], some: Image):
+    return min([(trained.number, distance(some, trained)) for trained in db], key=lambda x: x[1])
+
+
+def main():
+    db = list(read_train_data("train.csv"))
+    for image in read_test_data("test.csv"):
+        print(best_distance(db, image))
+
+
+if __name__ == '__main__':
+    main()

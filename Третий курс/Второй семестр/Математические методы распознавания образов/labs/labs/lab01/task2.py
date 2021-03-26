430b520235448efedaa079b88cb65722e0894f5b
Artur Khakimov <verybigfolder@yandex.ru>
2021-03-26 22:22:45 +0500

    Code reformat

--- Третий курс/Второй семестр/Математические методы распознавания образов/labs/labs/lab01/task2.py
+++ Третий курс/Второй семестр/Математические методы распознавания образов/labs/labs/lab01/task2.py
@@ -13,8 +13,7 @@ def main():
     train_images = list(read_train_data(docs / "train.csv"))
     db = {digit: [image for image in train_images if image.number == digit] for digit in range(10)}
     avg_vector_feature = {digit: np.mean(np.array([image.feature_vector for image in train_images]), axis=0) for
-                          (digit, train_images)
-                          in db.items()}
+                          (digit, train_images) in db.items()}
     for image in read_test_data(docs / "test.csv"):
         f, ax = min((KnownImage(image.data, digit, distance(image.feature_vector, avg_feature)) for (digit, avg_feature)
                      in avg_vector_feature.items()), key=lambda x: x.distance).visualize()

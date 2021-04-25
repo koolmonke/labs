8139585b1bb269e9af43cddc449033d47df6cb73
Arthur Khakimov <verybigfolder@yandex.ru>
2021-04-25 22:42:11 +0500

    Remember previous iteration size

--- Третий курс/Второй семестр/Недетерминированные алгоритмы/non-deterministic-algorithms/src/main/kotlin/lab1/lab1.kt
+++ Третий курс/Второй семестр/Недетерминированные алгоритмы/non-deterministic-algorithms/src/main/kotlin/lab1/lab1.kt
@@ -12,21 +12,20 @@ data class Item(val price: Int, val weight: Weight)
 
 typealias Items = List<Item>
 
-fun Items.currentSize() = this.map { it.weight }.sum()
-
 data class Backpack(val sizeOfBackpack: Weight) {
-    fun fill(items: Items): Items = items.sortedByDescending { it.weight / it.price }.fold(listOf()) { backpack, item ->
-        if (backpack.currentSize() + item.weight <= sizeOfBackpack)
-            backpack + item
-        else
-            backpack
-    }
+    fun fill(items: Items) =
+        items.sortedByDescending { it.weight / it.price }.fold(listOf<Item>() to 0) { backpack, item ->
+            if (backpack.second + item.weight <= sizeOfBackpack)
+                backpack.first + item to backpack.second + item.weight
+            else
+                backpack
+        }
 }
 
 fun main() {
     val filename = "docs/lab1/example.json"
     val backpack = Backpack(50).also { println(it) }
-    val backpackItems = backpack.fill(
+    val (backpackItems, totalWeight) = backpack.fill(
         Json.decodeFromString<Items>(File(filename).readText()).also { println(it) })
-    println("Предметы: $backpackItems, их вес ${backpackItems.currentSize()}")
+    println("Предметы: $backpackItems, их вес $totalWeight")
 }

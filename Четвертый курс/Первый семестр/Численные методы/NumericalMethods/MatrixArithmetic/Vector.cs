83a6bf4cc8f338db18a68937fbf9aa5c9801622a
Arthur Khakimov <verybigfolder@yandex.ru>
2021-09-22 16:19:21 +0500

    Имплементация вектора

---
+++ Четвертый курс/Первый семестр/Численные методы/NumericalMethods/MatrixArithmetic/Vector.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MatrixArithmetic
+{
+    public class Vector : IEnumerable<double>
+    {
+        IEnumerator<double> IEnumerable<double>.GetEnumerator() => ((IEnumerable<double>)Repr).GetEnumerator();
+
+        public IEnumerator GetEnumerator() => Repr.GetEnumerator();
+
+        public int N => Repr.Length;
+
+        public double this[Index i]
+        {
+            get => Repr[i];
+            set => Repr[i] = value;
+        }
+
+        public static Vector WithSize(int n) => new Vector(n);
+
+        public static Vector From(double[] values) => new Vector(values);
+
+        public Vector Copy() => From(Repr);
+
+        public static Vector operator *(Vector self, Vector other)
+        {
+            if (self.N != other.N)
+            {
+                throw new VectorDifferentDimException();
+            }
+
+            return self.Zip(other).Select(pair => pair.First * pair.Second).ToVector();
+        }
+
+        public static Vector operator *(Vector self, double other) => self.Select(value => other * value).ToVector();
+
+        public static Vector operator +(Vector self, Vector other)
+        {
+            if (self.N != other.N)
+            {
+                throw new VectorDifferentDimException();
+            }
+
+            return self.Zip(other).Select(pair => pair.First + pair.Second).ToVector();
+        }
+
+        public static Vector operator -(Vector self, Vector other)
+        {
+            if (self.N != other.N)
+            {
+                throw new VectorDifferentDimException();
+            }
+
+            return self.Zip(other).Select(pair => pair.First - pair.Second).ToVector();
+        }
+
+        public static Vector operator -(Vector self) => self.Select(value => -value).ToVector();
+
+
+        private double[] Repr;
+
+        private Vector(double[] values)
+        {
+            Repr = values.Select(value => value).ToArray();
+        }
+
+        private Vector(int n)
+        {
+            Repr = new double[n];
+        }
+    }
+}
---
+++ Четвертый курс/Первый семестр/Численные методы/NumericalMethods/MatrixArithmetic/VectorExt.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace MatrixArithmetic
+{
+    public static class VectorExt
+    {
+        public static Vector ToVector(this IEnumerable<double> enumerable) => Vector.From(enumerable.ToArray());
+
+        public static Matrix ToMatrix(this IEnumerable<double> enumerable)
+        {
+            var vector = enumerable.ToArray();
+
+            return Matrix.From(new[] { vector });
+        }
+    }
+}

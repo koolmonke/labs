f0d285104f4b18cdb19669f2262d2f94d713b319
Arthur Khakimov <verybigfolder@yandex.ru>
2021-10-16 18:32:45 +0500

    Refactor

--- Четвертый курс/Первый семестр/Параллельные алгоритмы/CSLabs/Lab3.Benchmark/Program.cs
+++ Четвертый курс/Первый семестр/Параллельные алгоритмы/CSLabs/Lab3.Benchmark/Program.cs
@@ -11,19 +11,19 @@ namespace Lab3.Benchmark
         private const int N = 100_000_000;
         private const int BatchSize = 100_000;
 
-        private (int[] array, int index)[] Data { get; }
+        private (int[] array, int index)[] BatchedData { get; }
 
         private int ItemToFind { get; }
 
-        private int[] GenData { get; }
+        private int[] Data { get; }
 
 
         public LinearVsBinary()
         {
             var random = new Random();
-            GenData = Lab3.Program.GenData(N);
-            ItemToFind = GenData[random.Next(GenData.Length)];
-            Data = GenData.OrderBy(x => x)
+            Data = Lab3.Program.GenData(N);
+            ItemToFind = Data[random.Next(Data.Length)];
+            BatchedData = Data.OrderBy(x => x)
                 .Batch(BatchSize)
                 .Select(item => item.ToArray())
                 .Select((array, index) => (array, index))
@@ -31,24 +31,24 @@ namespace Lab3.Benchmark
         }
 
         [Benchmark]
-        public int ParallelLinear() => Data.AsParallel()
+        public int ParallelLinear() => BatchedData.AsParallel()
             .Select(item => (IndexInInnerArray: item.array.MyLinearSearch(ItemToFind), ArrayIndex: item.index))
             .Where(item => item.IndexInInnerArray != -1)
             .Select(item => item.IndexInInnerArray + item.ArrayIndex * BatchSize)
             .First();
 
         [Benchmark]
-        public int ParallelBinary() => Data.AsParallel()
+        public int ParallelBinary() => BatchedData.AsParallel()
             .Select(item => (IndexInInnerArray: item.array.MyBinarySearch(ItemToFind), ArrayIndex: item.index))
             .Where(item => item.IndexInInnerArray != -1)
             .Select(item => item.IndexInInnerArray + item.ArrayIndex * BatchSize)
             .First();
 
         [Benchmark]
-        public int SequentialLinear() => GenData.MyLinearSearch(ItemToFind);
+        public int SequentialLinear() => Data.MyLinearSearch(ItemToFind);
 
         [Benchmark] 
-        public int SequentialBinary() => GenData.MyBinarySearch(ItemToFind);
+        public int SequentialBinary() => Data.MyBinarySearch(ItemToFind);
     }
 
     public static class Program

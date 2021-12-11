7d27a3712cd17b07c14f937de096ba79a31d8e75
Arthur Khakimov <verybigfolder@yandex.ru>
2021-12-12 01:52:08 +0500

    Удалил методы оптимизаций пока

--- Четвертый курс/Первый семестр/Методы оптимизаций/OptimizationMethods/Lab2/Enumeration.fs
+++
@@ -1,9 +0,0 @@
-module Lab2.Enumeration
-
-let argmin (start, finish) n func =
-    seq {
-        for i in 1 .. 2 .. 2 * n ->
-            start + (float i) * (finish - start) / float (2 * n)
-    }
-    |> Seq.map func
-    |> Seq.min
--- Четвертый курс/Первый семестр/Методы оптимизаций/OptimizationMethods/README.md
+++
@@ -1,6 +0,0 @@
-Мой вариант 42
-| № лабы | название файлы |
-| --- | --- |
-| 1 | UnimodalTsk |
-| 2 | MultiextrTsk |
-| 3 | MultiDimTsk |
